0633e08f6157894f48b853fe3c497ab3aa569c7f
zhangzhen <zzhang@gaojihealth.com>
2019-09-24 13:56:55 +0800

    增加springboot2.0依赖

--- src/main/java/com/zipkin/druid/filter/DruidFilter.java
+++ src/main/java/com/zipkin/druid/filter/DruidFilter.java
@@ -1,15 +1,12 @@
 package com.zipkin.druid.filter;
 
+import brave.Span;
+import brave.Tracer;
 import com.alibaba.druid.filter.FilterChain;
 import com.alibaba.druid.filter.FilterEventAdapter;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
 import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.Tracer;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -22,18 +19,18 @@ public class DruidFilter extends FilterEventAdapter {
 
     @Override
     public DruidPooledConnection dataSource_getConnection(FilterChain chain, DruidDataSource dataSource, long maxWaitMillis) throws SQLException {
-        Span span=tracer.createSpan("druid.connectPool.connect",tracer.getCurrentSpan());
+        Span span=tracer.nextSpan().name("druid.connectPool.connect").start();
         DruidPooledConnection druidPooledConnection=super.dataSource_getConnection(chain, dataSource, maxWaitMillis);
         span.tag("druid.activeCount",String.valueOf(dataSource.getActiveCount()));
-        tracer.close(span);
+        span.finish();
         return druidPooledConnection;
     }
     @Override
     public ConnectionProxy connection_connect(FilterChain chain, Properties info) throws SQLException {
         connection_connectBefore(chain, info);
-        tracer.createSpan("druid.connect",tracer.getCurrentSpan());
+        Span span=tracer.nextSpan().name("druid.connect").start();
         ConnectionProxy connection = super.connection_connect(chain, info);
-        tracer.close(tracer.getCurrentSpan());
+        span.finish();
         connection_connectAfter(connection);
 
         return connection;
--- src/main/java/com/zipkin/druid/starter/DruidTraceAutoConfiguration.java
+++ src/main/java/com/zipkin/druid/starter/DruidTraceAutoConfiguration.java
@@ -1,27 +1,14 @@
 package com.zipkin.druid.starter;
 
+import brave.Tracer;
 import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.zipkin.druid.filter.DruidFilter;
 import org.springframework.beans.BeansException;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.sleuth.TraceKeys;
-import org.springframework.cloud.sleuth.Tracer;
-import org.springframework.cloud.sleuth.autoconfig.SleuthProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.ArrayList;
